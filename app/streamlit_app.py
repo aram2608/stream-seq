a1b2630f33a43a6a6bd71f703bb4307ce9150509
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-13 21:24:00 -0400

    streamlit page scaffolding, tabs etc

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,29 @@
 import streamlit as st
+from pathlib import Path
+import pandas as pd
+from config import AnalysisConfig
+from orchestrator import run_r_engine
+from utils import save_uploaded_file, ensure_project_dirs, read_table_preview, which
 
-st.write("Hello world!")
+# Set the page title
+st.set_page_config(page_title="stream-seq (Python + R/DESeq2)", layout="wide")
+st.title("stream-seq — Python UI + R/DESeq2 Engine")
+
+# Set up out project root and associated directories for I/O
+project_root = Path(st.text_input("Project directory", value="project"))
+ensure_project_dirs(project_root)
+
+# Create tabs for I/O operations
+tab_input, tab_params, tab_run, tab_results = st.tabs(["Inputs", "Parameters", "Run", "Results"])
+
+with tab_input:
+    st.write("Hellow World!")
+
+with tab_params:
+    st.write("Hello World!")
+
+with tab_run:
+    st.write("Hello World!")
+
+with tab_results:
+    st.write("Hello world!")
